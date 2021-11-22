fb6fdfc8980dff6aa8b95e792b302f9555bb7aa9
Luthfi Munadzar <luthfimunadzar3@gmail.com>
2021-11-22 08:16:41 +0700

    add: google tag manager js

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,13 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vee-validate.js', ssr: true }],
+  plugins: [
+    { src: '~/plugins/vee-validate.js', ssr: true }, 
+    {
+        src: './plugins/vue-gtag.js',
+        mode: 'client'
+    }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
