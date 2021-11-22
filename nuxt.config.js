ddd150a61640a8ad0e3edc101679d91522c445e6
Luthfi Munadzar <luthfimunadzar3@gmail.com>
2021-11-22 12:23:27 +0700

    gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,10 +33,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vee-validate.js', ssr: true }, 
-    {
-        src: './plugins/vue-gtag.js',
-        mode: 'client'
-    }
+    { src: '~plugins/ga.js', mode: 'client' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
